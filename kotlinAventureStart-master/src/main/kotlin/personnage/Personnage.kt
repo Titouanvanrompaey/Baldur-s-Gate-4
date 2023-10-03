e505a6b38902e807fc41ec82298765ca8f4b0b7d
LD-BTS-SIO <122521972+LD-BTS-SIO@users.noreply.github.com>
2023-10-03 11:14:07 +0200

    Update Personnage.kt

--- kotlinAventureStart-master/src/main/kotlin/personnage/Personnage.kt
+++ kotlinAventureStart-master/src/main/kotlin/personnage/Personnage.kt
@@ -13,23 +13,54 @@ class Personnage(
     var defense: Int,
     var endurance: Int,
     var vitesse: Int,
-    var armure: Armure,
-    var armePrincipale: Arme,
-    var inventaire: MutableList<Item>) {
+    var armePrincipal: Arme?=null
 
+
+) {
+
+val hero = Personnage("hero", 20, 40, 12, 8, 10, 7)
      fun calculeDefense():Int{
          //TODO Mission 4.2
         return this.defense/2;
+
+
      }
-     // Méthode pour attaquer un adversaire
+
+
+    // Méthode pour attaquer un adversaire
      fun attaque(adversaire: Personnage) {
         //TODO Mission 4.1
-       val degats= this.attaque/2
+       var degats= this.attaque/2
+
+
+         if (armePrincipal != null){
+             degats= this.armePrincipal!!.calculDegat()
+
+
+        }
+         else{
+
+             println("$nom attaque ${adversaire.nom} avec une attaque de base et inflige $degats points de dégâts.")
+         }
+         var degats_infliges =degats-adversaire.calculeDefense()
+        println("Les dégats infligés sont {$degats_infliges}")
+         //
+
+         println("Le nombre de dégats est ${degats}, affligé par ${hero} ")
+         if (degats>1)
+         {
+             degats+=1
+         }
          adversaire.pointDeVie-=degats
-        println("$nom attaque ${adversaire.nom} avec une attaque de base et inflige $degats points de dégâts.")
+
     }
 
-    override fun toString(): String {
+    fun equipe(arme: Arme ) {
+
+        this.armePrincipal = arme
+        println("$nom equipe {$armePrincipal}")
+    }
+
         return "$nom (PV: $pointDeVie/$pointDeVieMax, Attaque: $attaque, Défense: $defense, Endurance: $endurance, Vitesse: $vitesse)"
     }
 }
