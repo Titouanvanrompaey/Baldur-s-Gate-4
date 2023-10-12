e4516b28655c194592834a1ebd725a78789fcfaf
Titouan van rompaey <116149382+Titouanvanrompaey@users.noreply.github.com>
2023-10-12 17:24:06 +0200

    Update Sort.kt

--- kotlinAventureStart-master/src/main/kotlin/item/Sort.kt
+++ kotlinAventureStart-master/src/main/kotlin/item/Sort.kt
@@ -1,13 +1,11 @@
 package item;
-import personnage.Mage
 import personnage.Personnage
-import jeu.Combat
-open class Sort(val nom: String,val effet :(Personnage,Personnage)->Unit, val puissance: Int, val coutMana: Int, val description: String) {
+
+open class Sort(
+    val nom: String,
+    val effet: (Personnage,Personnage) -> Unit, val puissance: Int=0, val coutMana: Int=0, val description: String="") {
     override fun toString(): String {
         return "$nom (Puissance: $puissance, Coût en mana: $coutMana)"
     }
 
 }
-
-
-
