64dd441afac8bddb54bc4d142f4699ee524571b3
scion <118342903+scioncarl@users.noreply.github.com>
2023-10-12 14:27:14 +0200

    Update Sort.kt

--- kotlinAventureStart-master/src/main/kotlin/item/Sort.kt
+++ kotlinAventureStart-master/src/main/kotlin/item/Sort.kt
@@ -1,11 +1,10 @@
 package item;
-
 import personnage.Mage
 import personnage.Personnage
-open class Sort(val nom: String, val coutMana: Int, val degats: Int) {
-    // Méthode pour lancer l'effet du sort sur une cible
-    fun lancerEffet(cible: Personnage) {
-        cible.pointsDeVie -= degats
-        println("${cible.nom} subit $degats points de dégâts.")
+import jeu.Combat
+open class Sort(val nom: String, val puissance: Int, val coutMana: Int, val description: String) {
+    override fun toString(): String {
+        return "$nom (Puissance: $puissance, Coût en mana: $coutMana)"
     }
+
 }
