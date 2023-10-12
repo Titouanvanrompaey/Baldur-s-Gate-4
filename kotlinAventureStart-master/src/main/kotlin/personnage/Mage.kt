37771defc2c47afe8da86877f9c0d5b4697d8e7c
Titouan van rompaey <116149382+Titouanvanrompaey@users.noreply.github.com>
2023-10-12 16:03:46 +0200

    Update Mage.kt

--- kotlinAventureStart-master/src/main/kotlin/personnage/Mage.kt
+++ kotlinAventureStart-master/src/main/kotlin/personnage/Mage.kt
@@ -5,55 +5,57 @@ import item.Sort
 import Item
 import personnage.Personnage
 class Mage(nom: String,
-             pointDeVie: Int,
-             pointDeVieMax: Int,
-             attaque: Int,
-             defense: Int,
-             endurance: Int? = null,
-             vitesse: Int,
-             armePrincipal: Arme? = null,
-             armure: Armure? = null,
-             inventaire: MutableList<Item> = mutableListOf())
+           pointDeVie: Int,
+           pointDeVieMax: Int,
+           attaque: Int,
+           defense: Int,
+           endurance: Int? = null,
+           vitesse: Int,
+           armePrincipal: Arme? = null,
+           armure: Armure? = null,
+           var grimoire:MutableList<Sort> = mutableListOf(),
+           inventaire: MutableList<Item> = mutableListOf())
     :Personnage(nom, pointDeVie, pointDeVieMax, attaque, defense, endurance, vitesse, armePrincipal, armure, inventaire)
-    {
-        fun afficherGrimoire() {
-            println("Affichage du grimoire du mage $nom :")
-            inventaire.filterIsInstance<Sort>().forEachIndexed { index, sort ->
-                println("${index + 1}. ${sort.nom} - Puissance : ${sort.puissance}, Coût en mana : ${sort.coutMana}")
-            }
-        }
-        fun choisirEtLancerSort() {
-            afficherGrimoire()
-            println("Veuillez choisir un sort en entrant son numéro d'index :")
-            val choixSort = readLine()?.toIntOrNull()
-
-            if (choixSort != null && choixSort in 0 until inventaire.size) {
-                val sortChoisi = inventaire[choixSort] as? Sort
-
-                if (sortChoisi != null) {
-                    println("Choisissez la cible du sort :")
-                    println("1 => Joueur")
-                    println("2 => Monstre")
-                    val choixCible = readLine()?.toIntOrNull()
-
-                    if (choixCible != null && (choixCible == 1 || choixCible == 2)) {
-                        val cible = if (choixCible == 1) {
-                            this
-                        } else {
-                            // Remplacez Monstre par la classe de votre monstre si nécessaire
-                            Monstre()
-                        }
-
-                        println("$nom lance ${sortChoisi.nom} sur ${cible.nom} !")
-                        sortChoisi.effet(this, cible) // Lance l'effet du sort
-                    } else {
-                        println("Choix de cible invalide. Veuillez choisir une cible valide.")
-                    }
-                } else {
-                    println("Cet objet n'est pas un sort. Veuillez choisir un sort valide.")
-                }
-            } else {
-                println("Index de sort invalide. Veuillez choisir un sort valide.")
-            }
+{
+    fun afficherGrimoire() {
+        var compteur=0
+        println("Affichage du grimoire du mage $nom :")
+        for (leSort in this.grimoire){
+            println("$compteur => ${leSort.nom}")
+            compteur+=1
         }
     }
+    fun choisirEtLancerSort(adv:Personnage) {
+        afficherGrimoire()
+        do {
+
+
+            println("Veuillez choisir un sort en entrant son numéro d'index :")
+            val choixSort = readln().toInt()
+
+
+            if (choixSort in 0 until grimoire.size) {
+                val sortChoisi = grimoire[choixSort]
+                println("Choisissez la cible du sort :")
+                println("1 => Joueur")
+                println("2 => Monstre")
+                val choixCible = readln().toInt()
+                if (choixCible == 1) {
+                    println("$nom lance ${sortChoisi.nom} sur ${this.nom} !")
+                    sortChoisi.effet(this, this)
+                }
+                else if (choixCible == 2) {
+                    println("$nom lance ${sortChoisi.nom} sur ${adv.nom} !")
+                    sortChoisi.effet(this, adv) // Lance l'effet du sort
+                }
+            } else {
+
+                    println("Index de sort invalide. Veuillez choisir un sort valide.")
+                    //rajouter le code du dessus pour qu'il puisse rechoisir un sort
+
+
+            }
+        }
+        while (choixSort < 0 || choixSort > inventaire.size)
+    }
+}
