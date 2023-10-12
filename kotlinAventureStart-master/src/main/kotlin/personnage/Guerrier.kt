4badcfeebcfa6b2501155a80d3d78728e613d6e9
LD-BTS-SIO <122521972+LD-BTS-SIO@users.noreply.github.com>
2023-10-12 16:23:54 +0200

    Update Guerrier.kt

--- kotlinAventureStart-master/src/main/kotlin/personnage/Guerrier.kt
+++ kotlinAventureStart-master/src/main/kotlin/personnage/Guerrier.kt
@@ -2,16 +2,75 @@ package personnage
 
 import item.Arme
 import item.Armure
-import Item
-import personnage.Personnage
-class Guerrier(nom: String,
-             pointDeVie: Int,
-             pointDeVieMax: Int,
-             attaque: Int,
-             defense: Int,
-             endurance: Int? = null,
-             vitesse: Int,
-             armePrincipal: Arme? = null,
-             armure: Armure? = null,
-             inventaire: MutableList<Item> = mutableListOf())
-    :Personnage(nom, pointDeVie, pointDeVieMax, attaque, defense, endurance, vitesse, armePrincipal, armure, inventaire)
+
+
+class Guerrier(
+    var armeSecondaire: Arme?,
+    nom: String,
+    description: String,
+    pointDeVie: Int,
+    pointDeVieMax: Int,
+    attaque: Int,
+    defense: Int,
+    endurance: Int,
+    vitesse: Int,
+    armePrincipal: Arme?,
+    inventaire: Armure?,
+) : Personnage(nom, pointDeVie, pointDeVieMax,attaque,defense, endurance, vitesse, armePrincipal, inventaire) {
+
+
+   override fun equipe(uneArme:Arme)
+   {
+println("Choisissez où vous voulez placer l'arme:")
+       var choix = readln().toInt()
+       var a = when(choix){
+            1 -> {
+                armePrincipal?.let { super.equipe(it) }
+            }
+            2 -> {
+                if (uneArme in this.inventaire) {
+                    this.armeSecondaire=uneArme
+                } else {
+                    println("Erreur de saisie")
+                }
+            }
+
+
+           else -> {
+              "Erreur de saisie"
+
+          }
+
+
+      }
+
+//      fun attaque(adversaire: Personnage)
+//       {
+//          var degats = this.attaque / 2
+//           fun totaDefense()
+//           {
+//               var degatot : Int
+//               if (armeSecondaire != null)
+//               {
+//              degatot = this.attaque - this.defense
+//               }
+//               else{
+//                   degatot =this.attaque - this.defense
+//               }
+//               if (degatot > 1)
+//               {
+//                   adversaire.pointDeVie -= degatot
+//                   println("Les dégats infligés sont {$degatot}")
+//               }
+//               else{
+//                   degatot = 1
+//               }
+      }
+
+
+       }
+
+
+
+
+
