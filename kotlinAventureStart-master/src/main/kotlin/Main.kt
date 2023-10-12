1295e3925bfd21a18894efd17abb6135426fbd03
Titouan van rompaey <116149382+Titouanvanrompaey@users.noreply.github.com>
2023-10-12 17:23:09 +0200

    Update Main.kt

--- kotlinAventureStart-master/src/main/kotlin/Main.kt
+++ kotlinAventureStart-master/src/main/kotlin/Main.kt
@@ -1,46 +1,67 @@
-import item.Qualite
-import item.Sort
+import item.*
 import jeu.Jeu
-import jeu.TirageDes
 import personnage.Personnage
+import jeu.TirageDes
 
 //instanciation des qualités des objets
 val qualiteCommun = Qualite("commun", 0, "\u001B[32m")
 val qualiteRare = Qualite("rare", 1, couleur = "\u001B[34m")
 val qualiteEpic = Qualite("epic", 2, "\u001B[35m")
 val qualiteLegendaire = Qualite("legendaire", 3, "\u001B[33m")
-val sortDeSoin = object : Sort("Sort de Soin", 80) {
-    override fun effet(caster: Personnage, cible: Personnage) {
-        val tirageDes = TirageDes(nbDe = 1, maxDe = 6)
-        var soin = tirageDes.lance()
-        soin = maxOf(a = 1, b = soin + 80)
-        cible.pointDeVie(soin)
-        println("Le sort de soin a soigné ${cible.nom} de $soin points de vie.")
+ val sortDeSoin =   Sort("Sort de Soin", { caster, cible->
+         run {
+             val tirageDes = TirageDes(nbDe = 1, maxDe = 6)
+             var soin = tirageDes.lance()
+             soin = maxOf(a = 1, b = soin + 80)
+             cible.pointDeVie+=soin
+             println("Le sort de soin a soigné ${cible.nom} de $soin points de vie.")
+         }
+     }
+ )
+val typeEpeeLongue= TypeArme("Epée longue", 2, 10, 60, 5 )
+val typeArmureCuir= TypeArmure("Armure en cuir", 3)
+val armeMagique= Sort("arme magique", { caster, cible ->
+    run {
+        val tirageDes=TirageDes(nbDe = 1, maxDe = 20)
+        var resultat=tirageDes.lance()
+        var qualite=qualiteCommun
+        if (resultat<5)
+            qualite=qualiteCommun
+        else if (resultat<10)
+            qualite=qualiteRare
+        else if (resultat<15)
+            qualite=qualiteEpic
+        else
+            qualite=qualiteLegendaire
+        var arme= Arme("armeMagique", typeEpeeLongue, qualite ,"une arme magique" )
+        cible.inventaire.add(arme)
+        println("Une arme magique a été ajoutée à l'inventaire.")
+        cible.armePrincipal=arme
+    }
+})
+val armureMagique= Sort("armure magique", { caster, cible ->
+    run {
+        val tirageDes = TirageDes(nbDe = 1, maxDe = 20)
+        var resultat = tirageDes.lance()
+        var qualite = qualiteCommun
+        if (resultat < 5)
+            qualite = qualiteCommun
+        else if (resultat < 10)
+            qualite = qualiteRare
+        else if (resultat < 15)
+            qualite = qualiteEpic
+        else
+            qualite = qualiteLegendaire
+        var armure = Armure("armure en cuir", "une armure bien sérrée", typeArmureCuir, qualite)
+        cible.inventaire.add(armure)
+        println("Une armure magique a été ajoutée à l'inventaire.")
+        cible.armure = armure
     }
 }
-val armeMagique = object : Arme("Épée Magique", 50) {
-    override fun attaquer(cible: Personnage) {
-        val degats = this.puissance - cible.defense
-        if (degats > 0) {
-            cible.recevoirDegats(degats)
-            println("${this.nom} inflige $degats points de dégâts à ${cible.nom} !")
-        } else {
-            println("${this.nom} est inefficace contre ${cible.nom} à cause de sa défense élevée !")
-        }
-    }
-}
-val armureMagique = object : Armure("Armure Magique", 30) {
-    override fun encaisserDegats(degats: Int) {
-        val degatsReduits = maxOf(1, degats - this.defense)
-        println("${this.nom} absorbe $degatsReduits points de dégâts !")
-    }
-}
-
+)
 
 fun monstres(args: Array<String>) {
     //Instantiation des monstres
-    // TODO Intermission 1 Ajouter d'autres monstres
-    //On ajoute les monstres a la liste de monstres du jeu
     val gobelin = Personnage(
         "Damien",
         pointDeVie = 40,
@@ -59,7 +80,7 @@ fun monstres(args: Array<String>) {
         vitesse = 6,
         endurance = 8)
 
-    val Minotor_squelette = Personnage(
+    val Minotaure_squelette = Personnage(
         "vasar",
         pointDeVie = 120,
         pointDeVieMax = 120,
@@ -76,8 +97,9 @@ fun monstres(args: Array<String>) {
         defense = 23,
         vitesse = 15,
         endurance = 9)
-    
-    val jeu = Jeu(listOf( gobelin, Basilic, Minotor_squelette, Yeenoghu))
+    // TODO Intermission 1 Ajouter d'autres monstres
+    //On ajoute les monstres a la liste de monstres du jeu
+    val jeu = Jeu(listOf( gobelin, Basilic, Minotaure_squelette, Yeenoghu))
     //Lancement du jeu
     jeu.lancerCombat()
 }
